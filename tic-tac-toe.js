fd2e3503edac9baf2af7361c65f0cea5b712e517
xiejon <jxie77@gmail.com>
2022-05-15 20:22:20 +0100

    added event listeners that add X or O to grid

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,38 +1,60 @@
+const displayController = (() => {
+})();
+
 const gameBoard = (() => {
     let board = [];
+    let turn = 0;
     const container = document.querySelector('.container');
 
-    // create basic empty board
+    // create basic empty board with attributes and event listeners
     function createBoard() {
         for (let i = 0; i < 9; i++) {
             const box = document.createElement('div');
+            box.classList.add('.box');
             container.appendChild(box);
             board.push(box);
-
-            // add row, column, diagonal attributes
-            if (i < 3) {
-                if (i === 0) board[i].diag0 = 0;
-                if (i === 2) board[i].diag1 = 0;
-                board[i].row = 0;
-                board[i].column = i;
-            } else if (i >= 3 && i < 6) {
-                if (i === 4) {
-                    board[i].diag0 = 0;
-                    board[i].diag1 = 0;
-                }
-                board[i].row = 1;
-                board[i].column = i - 3;
-            } else if (i >= 6 && i < 9) {
-                if (i === 6) board[i].diag1 = 0;
-                if (i === 8) board[i].diag0 = 0;
-                board[i].row = 2;
-                board[i].column = i - 6;
-            }
+            addAttributes(i);
+            addClickListener(box);
         }
     }
 
+    function addAttributes(index) {
+        if (index < 3) {
+            if (index === 0) board[index].diag0 = 0;
+            if (index === 2) board[index].diag1 = 0;
+            board[index].row = 0;
+            board[index].column = index;
+        } else if (index >= 3 && index < 6) {
+            if (index === 4) {
+                board[index].diag0 = 0;
+                board[index].diag1 = 0;
+            }
+            board[index].row = 1;
+            board[index].column = index - 3;
+        } else if (index >= 6 && index < 9) {
+            if (index === 6) board[index].diag1 = 0;
+            if (index === 8) board[index].diag0 = 0;
+            board[index].row = 2;
+            board[index].column = index - 6;
+        }
+    }
+
+    function addClickListener(item) {
+        item.addEventListener('click', userSelection);
+    }
+
+    function userSelection() {
+        if (turn === 0) {
+            this.textContent = 'X';
+            turn = 1;
+        } else if (turn === 1) {
+            this.textContent = 'O';
+            turn = 0;
+        }
+        console.log('hello');
+    }
+
     createBoard();
-    // 
 
     return {
         createBoard: createBoard,
@@ -40,9 +62,10 @@ const gameBoard = (() => {
     };
 })();
 
-const displayController = (() => {
 
-})();
+const Player = (playerNum) => {
+
+}
 
 let newBoard = gameBoard.board;
 // console.log(gameBoard.board);
